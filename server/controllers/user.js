09be3e91dc7ead34dacb88d05d62c9a5bf0e6105
CactusField <jonathanpitts2913@gmail.com>
2021-01-20 11:05:56 -0600

    init

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,11 +2,13 @@ const bcrypt = require('bcryptjs')
 const session = require('express-session')
 
 module.exports={
+    
+    
     register: async (req,res)=>{
         const {username,password}=req.body
         const db= req.app.get('db')
         const [userFound]= await db.user.find_user_by_username(username)
-        if(userFound) return res.status(409).send("username exists already")
+        if(userFound) return res.status(409).send("Username is already Taken :(")
         const salt = bcrypt.genSaltSync(10)
         const hash = bcrypt.hashSync(password,salt)
         const pic=`https://robohash.org/${username}.png`
@@ -16,26 +18,29 @@ module.exports={
         
         res.status(201).send(req.session.user)
     },
+    
+    
     login: async (req,res)=>{
         const {username,password}=req.body        
         const db= req.app.get('db')
         const [userFound]= await db.user.find_user_by_username(username)
-        if(!userFound)return res.status(404).send('No user by that name here')       
+        if(!userFound)return res.status(404).send('Cant find that User!')       
         const isAuth= bcrypt.compareSync(password, userFound.password)
-        if(!isAuth) return res.status(403).send('password is incorrect')        
+        if(!isAuth) return res.status(403).send('Not the right password')        
         req.session.user=userFound        
-        res.status(200).send(req.session.user)
-        
+        res.status(200).send(req.session.user)        
     },
+    
+    
     logout:  (req,res)=>{          
         req.session.destroy()
         res.sendStatus(200)
     },
+    
+    
     getUser:(req,res)=>{      
         console.log(req.session)          
         if(req.session) return res.status(200).send(req.session.user)        
-        res.status(404).send('No session found')
-
-      
+        res.status(404).send('No session') 
     },
 }
